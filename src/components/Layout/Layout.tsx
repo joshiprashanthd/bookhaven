3b8ede3615b4664900d4b455cd40f11fc1f9bc4b
prashant joshi <joshiprashanthd121@gmailcom>
2023-09-29 16:14:54 +0530

    add: Layout component to center contents

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Container } from "@chakra-ui/react"
+import React from "react"
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <Container centerContent maxW="container.lg">
+            {children}
+        </Container>
+    )
+}
