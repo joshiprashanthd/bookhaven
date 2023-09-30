970d1e8d3222ba2fae14b5e3b7024fde21b4220d
prashant joshi <joshiprashanthd121@gmailcom>
2023-09-30 21:56:46 +0530

    fix: remove unused properties and use variant prop for input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,40 +1,26 @@
 "use client"
-import { Flex, HStack, IconButton, Input } from "@chakra-ui/react"
-import { Layout } from "../Layout/Layout"
+import { Flex, IconButton, Input } from "@chakra-ui/react"
 import { SearchIcon } from "@/icons/SearchIcon"
 
 export const Search = () => {
     return (
-        <Layout>
-            <HStack minW="full" h="10" overflow="hidden" spacing="4">
-                <Flex
-                    justify="space-between"
-                    h="full"
-                    align="center"
-                    flex="1"
-                    my={4}
-                    py={2}
-                    px={4}
-                    bg="gray.50"
-                    borderRadius={8}
-                >
-                    <Input
-                        p={0}
-                        placeholder="Search"
-                        border={0}
-                        __css={{
-                            decoration: "none",
-                        }}
-                        _focus={{
-                            border: "none",
-                        }}
-                        _focusVisible={{
-                            border: "none",
-                        }}
-                    />
-                    <IconButton aria-label="search-button" icon={<SearchIcon />} bg="none" />
-                </Flex>
-            </HStack>
-        </Layout>
+        <Flex
+            justify="space-between"
+            h="10"
+            align="center"
+            my={4}
+            py={2}
+            pl={4}
+            bg="gray.50"
+            borderRadius={8}
+        >
+            <Input variant="unstyled" placeholder="Search" />
+            <IconButton
+                aria-label="search-button"
+                icon={<SearchIcon />}
+                bg="none"
+                borderRadius={8}
+            />
+        </Flex>
     )
 }
