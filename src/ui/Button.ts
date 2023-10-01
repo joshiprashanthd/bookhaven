bf189a05e68b403d2bb0ddfedcf8c063356c7127
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-02 01:52:02 +0530

    ui: update Button theme

--- src/ui/Button.ts
+++ src/ui/Button.ts
@@ -4,16 +4,42 @@ export const Button = defineStyleConfig({
     baseStyle: {
         borderRadius:8,
         textTransform: "uppercase",
-        fontWeight: "bold"
+        fontWeight: "bold",
+    },
+    sizes: {
+        sm: {
+            fontSize: 10,
+        },
+        md: {
+            fontSize: 12
+        }
     },
     variants: {
-        filled: {
+        primary: {
             backgroundColor: "purple.500",
-            color: "white"
+            color: "white",
+            _hover: {
+                backgroundColor: "purple.400"
+            }
+        },
+        secondary: {
+            backgroundColor: "none",
+            borderWidth: 2,
+            borderColor: "purple.400",
+            _hover: {
+                backgroundColor: "purple.50"
+            }
+        },
+        tertiary: {
+            backgroundColor: "gray.50",
+            color: "black",
+            _hover: {
+                backgroundColor: "gray.100"
+            }
         }
     },
     defaultProps: {
         size: "md",
-        variant: "filled"
+        variant: "primary"
     }
 })
