ab5968e92cfe5717be8cc3f42e04b24927160355
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-02 02:28:32 +0530

    add heading to filter section

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-import { VStack } from "@chakra-ui/react"
+import { Heading, VStack } from "@chakra-ui/react"
 import React from "react"
 import { YearFilter } from "./YearFilter"
 import { GenreFilter } from "./GenreFilter"
 
 export const Filter = () => {
     return (
-        <VStack spacing={4} bg="gray.100" borderRadius={16} minW="72" p={4}>
+        <VStack spacing={4} bg="gray.100" borderRadius={16} minW="72" p={4} align="start">
+            <Heading size="md" fontWeight="semibold">
+                Filters
+            </Heading>
             <YearFilter />
             <GenreFilter />
         </VStack>
