ea06a073c068bef99f1f0d99857dd465f0ff60fb
prashant joshi <joshiprashanthd121@gmailcom>
2023-10-01 10:56:00 +0530

    make GenreFilter functional

--- src/components/Filter/GenreFilter.tsx
+++ src/components/Filter/GenreFilter.tsx
@@ -1,9 +1,33 @@
-import { VStack, Text } from "@chakra-ui/react"
+import { ChevronDown } from "@/icons/ChevronDown"
+import { Button, Menu, MenuButton, MenuItem, MenuList, Text, VStack } from "@chakra-ui/react"
+import { useState } from "react"
 
 export const GenreFilter = () => {
+    const options = ["SciFi", "Horror", "Mystery", "Romance", "Adventure", "Kids"]
+    const defaultName = "SciFi"
+    const [selected, setSelected] = useState(defaultName)
+
     return (
-        <VStack>
+        <VStack align="start" w="full">
             <Text>Select genre</Text>
+            <Menu>
+                <MenuButton
+                    w="full"
+                    as={Button}
+                    rightIcon={<ChevronDown />}
+                    bg="gray.50"
+                    borderRadius={8}
+                >
+                    {selected}
+                </MenuButton>
+                <MenuList>
+                    {options.map((item) => (
+                        <MenuItem key={item} onClick={() => setSelected(item)}>
+                            {item}
+                        </MenuItem>
+                    ))}
+                </MenuList>
+            </Menu>
         </VStack>
     )
 }
