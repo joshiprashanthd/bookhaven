68edfe758b7147b525fcaf8296bdfc387bb07a94
prashant joshi <joshiprashanthd121@gmailcom>
2023-10-01 11:32:00 +0530

    change: ui of YearFilter is replaces with modern menu

--- src/components/Filter/YearFilter.tsx
+++ src/components/Filter/YearFilter.tsx
@@ -1,33 +1,67 @@
 import {
-    VStack,
+    Button,
+    HStack,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
     Text,
-    RangeSlider,
-    RangeSliderTrack,
-    RangeSliderFilledTrack,
-    RangeSliderThumb,
+    VStack,
 } from "@chakra-ui/react"
 import { useState } from "react"
 
 export const YearFilter = () => {
     const [range, setRange] = useState([2000, 2000])
     return (
-        <VStack w="full">
-            <Text fontWeight="semibold">
-                Selected Range: {range[0]} to {range[1]}
-            </Text>
-            <RangeSlider
-                defaultValue={[2000, 2000]}
-                min={2000}
-                max={2023}
-                step={1}
-                onChange={setRange}
-            >
-                <RangeSliderTrack bg="purple.100">
-                    <RangeSliderFilledTrack bg="purple.500" />
-                </RangeSliderTrack>
-                <RangeSliderThumb boxSize={4} index={0} />
-                <RangeSliderThumb boxSize={4} index={1} />
-            </RangeSlider>
+        <VStack w="full" align="start">
+            <Text>Select year range</Text>
+            <HStack w="full">
+                <YearMenu
+                    selected={range[0]}
+                    min={2000}
+                    max={2023}
+                    onChange={(select) => setRange((range) => [select, range[1]])}
+                />
+                <Text>To</Text>
+                <YearMenu
+                    selected={range[1] >= range[0] ? range[1] : range[0]}
+                    min={range[0]}
+                    max={2023}
+                    onChange={(select) => setRange((range) => [range[0], select])}
+                />
+            </HStack>
         </VStack>
+    )
+}
+
+const YearMenu = ({
+    selected,
+    min,
+    max,
+    onChange,
+}: {
+    selected: number
+    min: number
+    max: number
+    onChange: (arg: number) => void
+}) => {
+    const range = []
+    for (let i = min; i <= max; i++) {
+        range.push(i)
+    }
+
+    return (
+        <Menu>
+            <MenuButton as={Button} w="full" bg="gray.50" borderRadius={8}>
+                {selected}
+            </MenuButton>
+            <MenuList maxH="64" overflow="scroll" borderRadius={8} w="full">
+                {range.map((item) => (
+                    <MenuItem key={item} onClick={() => onChange(item)}>
+                        {item}
+                    </MenuItem>
+                ))}
+            </MenuList>
+        </Menu>
     )
 }
