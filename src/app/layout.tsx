c9f95dd4a426397307afa976d3fce7a349da4d34
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-02 10:26:51 +0530

    clean: unused Inter package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
-import { Filter } from "@/components/Filter/Filter"
 import { Layout } from "@/components/Layout/Layout"
 import { Nav } from "@/components/Nav/Nav"
-import { Flex } from "@chakra-ui/react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import { Providers } from "./providers"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
     title: "Bookhaven",
@@ -16,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body>
                 <Providers>
                     <Nav />
                     <Layout>{children}</Layout>
