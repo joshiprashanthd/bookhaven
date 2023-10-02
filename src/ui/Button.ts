12b778559cdcce5f43e59e9c9a1b4bea4f3f53db
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-02 09:34:49 +0530

    specify height for big and normal sizes of Button

--- src/ui/Button.ts
+++ src/ui/Button.ts
@@ -1,45 +1,49 @@
-import { defineStyleConfig } from "@chakra-ui/react";
+import { defineStyleConfig } from "@chakra-ui/react"
 
 export const Button = defineStyleConfig({
     baseStyle: {
-        borderRadius:8,
+        borderRadius: 8,
         textTransform: "uppercase",
         fontWeight: "bold",
     },
     sizes: {
-        sm: {
-            fontSize: 10,
+        small: {
+            h: 8,
+            p: 4,
+            fontSize: 12,
         },
-        md: {
-            fontSize: 12
-        }
+        normal: {
+            h: 10,
+            p: 4,
+            fontSize: 14,
+        },
     },
     variants: {
         primary: {
             backgroundColor: "purple.500",
             color: "white",
             _hover: {
-                backgroundColor: "purple.400"
-            }
+                backgroundColor: "purple.400",
+            },
         },
         secondary: {
             backgroundColor: "none",
             borderWidth: 2,
             borderColor: "purple.400",
             _hover: {
-                backgroundColor: "purple.50"
-            }
+                backgroundColor: "purple.50",
+            },
         },
         tertiary: {
             backgroundColor: "gray.50",
             color: "black",
             _hover: {
-                backgroundColor: "gray.100"
-            }
-        }
+                backgroundColor: "gray.100",
+            },
+        },
     },
     defaultProps: {
-        size: "md",
-        variant: "primary"
-    }
+        size: "normal",
+        variant: "primary",
+    },
 })
