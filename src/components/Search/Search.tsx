f75b560771836cc2ee2bb51156e3419a9011eb38
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-04 11:37:53 +0530

    pass search params on apply clicking search button

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,19 @@
 "use client"
+
 import { Flex, IconButton, Input } from "@chakra-ui/react"
 import { SearchIcon } from "@/icons/SearchIcon"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 export const Search = () => {
+    const [query, setQuery] = useState("")
+    const router = useRouter()
+
+    const onChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setQuery(e.target.value)
+    }
+
     return (
         <Flex
             justify="space-between"
@@ -14,7 +25,7 @@ export const Search = () => {
             bg="gray.50"
             borderRadius={8}
         >
-            <Input variant="unstyled" placeholder="Search" />
+            <Input variant="unstyled" placeholder="Search" onChange={onChangeQuery} />
             <IconButton
                 h={10}
                 w={12}
@@ -23,6 +34,7 @@ export const Search = () => {
                 icon={<SearchIcon />}
                 borderRightRadius={8}
                 borderLeftRadius={0}
+                onClick={() => router.push(`/search?q=${query}`)}
             />
         </Flex>
     )
