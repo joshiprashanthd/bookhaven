7cf3cf98c20ad3c827255c0c129389ffac742423
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-06 19:44:34 +0530

    update parameters for icons

--- src/icons/UserIcon.tsx
+++ src/icons/UserIcon.tsx
@@ -4,7 +4,7 @@ export const UserIcon = ({ size = 24 }: { size?: number }) => {
             xmlns="http://www.w3.org/2000/svg"
             width={size}
             height={size}
-            viewBox={`0 0 ${size} ${size}`}
+            viewBox={`0 0 24 24`}
             fill="none"
             stroke="currentColor"
             strokeWidth="2"
