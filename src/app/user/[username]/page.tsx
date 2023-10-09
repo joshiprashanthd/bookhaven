3f9a67f2756f7119d52a3829e291345dc753811b
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-09 12:18:21 +0530

    change route parameter to username and stuff to user page

---
+++ src/app/user/[username]/page.tsx
@@ -0,0 +1,26 @@
+import { Box, Text, HStack, VStack, Heading } from "@chakra-ui/react"
+
+export default function ProfilePage({ params }: { params: { username: string } }) {
+    return (
+        <Box as="section" w="full">
+            <HStack align="start" spacing={4}>
+                <VStack borderRadius={16} bg="gray.100" p={8} w="20vw" align="start">
+                    <Box h="52" w="52" borderRadius="full" bg="gray.500" mx="auto" mb={4}></Box>
+                    <Heading size="lg">Prashant Joshi</Heading>
+                    <Text color="purple.500">{params.username}</Text>
+                    <VStack align="start" spacing="-4">
+                        <Text fontWeight="bold">About ME</Text>
+                        <Text fontSize="14">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe maxime
+                            doloremque atque est modi adipisci! Tenetur eius asperiores error
+                            delectus dolore repellat obcaecati aut, enim dicta.
+                        </Text>
+                    </VStack>
+                    <Text fontSize="14">Location : India</Text>
+                    <Text fontSize="14">Personal Site : something.com</Text>
+                </VStack>
+                <VStack flex="1" bg="gray.100" borderRadius={16} p={8} minH="100vh"></VStack>
+            </HStack>
+        </Box>
+    )
+}
