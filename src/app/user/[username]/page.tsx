0761560f9fbf3efb2fbb9166342897a3ea62ef6a
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-10-12 14:04:00 +0530

    change padding to 4 units

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -7,7 +7,7 @@ export default function ProfilePage({ params }: { params: { username: string } }
     return (
         <Box as="section" w="full">
             <HStack align="start" spacing={4}>
-                <VStack borderRadius={16} bg="gray.100" p={8} w="20vw" align="start">
+                <VStack borderRadius={16} bg="gray.100" p={4} w="20vw" align="start">
                     <Box h="52" w="52" borderRadius="full" bg="gray.500" mx="auto" mb={4}></Box>
                     <Heading size="lg">Prashant Joshi</Heading>
                     <Text color="purple.500">{params.username}</Text>
