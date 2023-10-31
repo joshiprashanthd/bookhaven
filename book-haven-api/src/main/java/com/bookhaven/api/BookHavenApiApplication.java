c1c9e63feda549a17020f0cd751c52f1312cccdc
Ankit123Mishra <ankit.mishra682@gmail.com>
2023-10-31 22:05:47 +0530

    Adding CORS filter

--- book-haven-api/src/main/java/com/bookhaven/api/BookHavenApiApplication.java
+++ book-haven-api/src/main/java/com/bookhaven/api/BookHavenApiApplication.java
@@ -5,12 +5,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BookHavenApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookHavenApiApplication.class, args);
+    }
+
+    @Bean FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
+        return registrationBean;
     }
 
     @Bean
