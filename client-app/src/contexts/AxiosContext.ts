e2a2247aa4f9a35c83ab4c5980d9bbc8c60a19f6
Ankit123Mishra <ankit.mishra682@gmail.com>
2023-11-26 01:42:17 +0530

    Ported client app from Next js to React js

--- client-app/src/contexts/AxiosContext.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-import { createContext } from "react"
-
-export const AxiosContext = createContext(
-    axios.create({
-        baseURL: "http://localhost:8080/api",
-        withCredentials: true,
-    })
-)
