f642d9aa9fac70797c9f595077abc13625047f4b
prashant joshi <joshiprashanthd121@gmail.com>
2023-11-25 19:28:44 +0530

    ui: add rating indicator

--- client-app/src/components/Review/Review.tsx
+++ client-app/src/components/Review/Review.tsx
@@ -1,18 +1,38 @@
+import { StarIcon } from "@/icons/StarIcon"
 import { UserIcon } from "@/icons/UserIcon"
-import { Box, HStack, VStack, Text } from "@chakra-ui/react"
+import { Box, HStack, VStack, Text, Flex } from "@chakra-ui/react"
 
 export const Review = ({}) => {
     return (
         <Box borderRadius={16} bg="gray.50" p={4}>
             <VStack align="start">
-                <HStack mb={2}>
-                    <Box w={10} h={10} mr={2} display="grid" placeItems="center">
-                        <UserIcon size={18} />
-                    </Box>
-                    <Text fontSize={14} fontWeight={600} color="purple.500">
-                        Ankit Mishra
-                    </Text>
-                </HStack>
+                <Flex mb={2} justify="space-between" w="full">
+                    <HStack>
+                        <Box
+                            w={10}
+                            h={10}
+                            mr={1}
+                            display="grid"
+                            placeItems="center"
+                            borderRadius="full"
+                            bg="gray.400"
+                        >
+                            <UserIcon size={18} />
+                        </Box>
+                        <Text fontSize={14} fontWeight={600} color="purple.500">
+                            Ankit Mishra
+                        </Text>
+                    </HStack>
+
+                    <HStack>
+                        <Text>4</Text>
+                        <StarIcon
+                            size={18}
+                            fill="rgb(128, 90, 213)"
+                            strokeColor="rgb(128, 90, 213)"
+                        />
+                    </HStack>
+                </Flex>
                 <Box>
                     <Text fontSize={14}>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat quos cum
